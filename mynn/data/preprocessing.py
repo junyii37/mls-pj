dd3b52f7bc953350499acf6849507d3b9cdc39d1
Richardsrj <155440407+Richardsrj@users.noreply.github.com>
2025-05-19 20:30:30 +0800

    Update preprocessing.py

--- mynn/data/preprocessing.py
+++ mynn/data/preprocessing.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+import pickle
 
 
 def preprocess(dataset):
@@ -24,3 +26,33 @@ def preprocess(dataset):
         labels_onehot[n][_class] = 1
 
     return images, labels_onehot
+
+
+def load_cifar10_batch(filename):
+    with open(filename, 'rb') as f:
+        batch = pickle.load(f, encoding='bytes')
+        images = batch[b'data'].reshape(-1, 3, 32, 32)
+        labels = np.array(batch[b'labels'])
+        labels_onehot = np.zeros((len(labels), 10))
+        for n, label in enumerate(labels):
+            labels_onehot[n][label] = 1
+
+        return images, labels_onehot
+
+
+def load_cifar10_dataset(data_dir):
+    train_images = []
+    train_labels = []
+    for i in range(1, 6):
+        batch_file = os.path.join(data_dir, f'data_batch_{i}')
+        imgs, lbls = load_cifar10_batch(batch_file)
+        train_images.append(imgs)
+        train_labels.append(lbls)
+    train_images = np.concatenate(train_images, axis=0)
+    train_labels = np.concatenate(train_labels, axis=0)
+
+    # 加载测试集
+    test_file = os.path.join(data_dir, 'test_batch')
+    test_images, test_labels = load_cifar10_batch(test_file)
+
+    return train_images/255.0, train_labels, test_images/255.0, test_labels
