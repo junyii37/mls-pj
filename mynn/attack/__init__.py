4e6a440bce449e7f49c1db41c936981593fb03ec
Richardsrj <155440407+Richardsrj@users.noreply.github.com>
2025-05-23 13:04:44 +0800

    Update __init__.py

--- mynn/attack/__init__.py
+++ mynn/attack/__init__.py
@@ -1,2 +1,3 @@
-from .fgsm_attack import fgsm_attack, generate_adversarial_batch_fgsm
+from .fgsm_attack import *
 from .BIM_attack import bim_attack, generate_adversarial_batch_bim
+from .trades import *
