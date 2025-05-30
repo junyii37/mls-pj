544b194440e9904b1d6d0d475268dbd016e35321
WangChun <2430837552@qq.com>
2025-05-30 10:56:19 +0800

    Update runner.py
    加了pgdattack

--- mynn/runner/runner.py
+++ mynn/runner/runner.py
@@ -9,6 +9,35 @@ from tqdm import tqdm
 
 from mynn.data import dataloader
 from ..attack import generate_adversarial_batch_bim, generate_adversarial_batch_fgsm
+from mynn.loss import CrossEntropy,TRADESLoss
+from mynn.attack import pgd_kl_attack
+
+
+import torch
+import torch.nn as nn
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def pgd_attack(model, images, labels, eps=0.3, alpha=2 / 255, iters=40):
+    images = images.to(device)
+    labels = labels.to(device)
+    loss = nn.CrossEntropyLoss()
+
+    ori_images = images.data
+
+    for i in range(iters):
+        images.requires_grad = True
+        outputs = model(images)
+
+        model.zero_grad()
+        cost = loss(outputs, labels).to(device)
+        cost.backward()
+
+        adv_images = images + alpha * images.grad.sign()
+        eta = torch.clamp(adv_images - ori_images, min=-eps, max=eps)
+        images = torch.clamp(ori_images + eta, min=0, max=1).detach_()
+
+    return images
+
+
 
 class RunnerM:
     """
@@ -113,20 +142,16 @@ class RunnerM:
         num_epochs = kwargs.get('num_epochs', 50)
         scheduler = kwargs.get('scheduler', None)
         strategy = kwargs.get('strategy', None)
-        epsilon = kwargs.get('epsilon', 2.0 / 255.0)
+        epsilon = kwargs.get('epsilon', 1.0/255)
         shuffle = kwargs.get('shuffle', True)
-        attack_strategy = kwargs.get('attack_strategy','fgsm')
+        attack_strategy = kwargs.get('attack_strategy', 'fgsm')
         save_dir = Path(kwargs.get('save_dir', 'saved_models'))
-        num_steps = kwargs.get("num_steps", 5)
-        step_size = kwargs.get("step_size", 0.5 / 255)
 
         if attack_strategy == 'bim':
             generate_adversarial_batch = generate_adversarial_batch_bim
         elif attack_strategy == 'fgsm':
             generate_adversarial_batch = generate_adversarial_batch_fgsm
-
         # else if
-
         self.results.clear()
         now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         self.path = save_dir / now
@@ -150,7 +175,7 @@ class RunnerM:
                     y_target = y_batch[half:]
 
                     # 动态生成对抗样本
-                    x_adv = generate_adversarial_batch(self.model, x_target, y_target, self.loss, epsilon, num_steps, step_size)
+                    x_adv = generate_adversarial_batch(self.model, x_target, y_target, self.loss,epsilon)
 
                     # 合并 clean + adv
                     X_aug = cp.concatenate([x_clean, x_adv], axis=0)
@@ -197,7 +222,6 @@ class RunnerM:
                 break
 
         self.plot()
-
     def train_with_trade(self, train_set, dev_set, **kwargs):
         batch_size = kwargs.get('batch_size', 128)
         num_epochs = kwargs.get('num_epochs', 50)
@@ -299,13 +323,31 @@ class RunnerM:
             Xb = cp.asarray(X_batch)
             yb = cp.asarray(y_batch)
             logits = self.model(Xb, train=False)
-            loss_val, acc_val = loss_fn(logits, yb)
+            loss_val, acc_val = self.loss(logits, yb)
             loss_list.append(float(loss_val))
             acc_list.append(float(acc_val))
         avg_loss = sum(loss_list) / len(loss_list) if loss_list else 0.0
         avg_acc  = sum(acc_list)  / len(acc_list)  if acc_list  else 0.0
         return avg_loss, avg_acc
+    
+    # 针对PGD
+    def evaluate2(self, dataset, batch_size=256,loss_fn=None):
+        if loss_fn is None:
+            loss_fn=self.loss
+        loss_list, acc_list = [], []
+        for X_batch, y_batch in dataloader(dataset, batch_size=batch_size, shuffle=False):
+            Xb = cp.asarray(X_batch)
+            yb = cp.asarray(y_batch)
 
+            Xb = pgd_attack(self.model, Xb, yb)
+
+            logits = self.model(Xb, train=False)
+            loss_val, acc_val = self.loss(logits, yb)
+            loss_list.append(float(loss_val))
+            acc_list.append(float(acc_val))
+        avg_loss = sum(loss_list) / len(loss_list) if loss_list else 0.0
+        avg_acc  = sum(acc_list)  / len(acc_list)  if acc_list  else 0.0
+        return avg_loss, avg_acc
     def save_model(self, save_path):
         self.model.save_model(save_path)
 
